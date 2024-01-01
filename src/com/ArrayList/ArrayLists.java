8d69c906f5a9fd96387285b9de1b28d8632e00f2
MohIrsadDeveloper <idrisimohdirshad@gmail.com>
2024-01-01 18:15:00 +0530

    Added ArrayLists

--- src/com/ArrayList/ArrayLists.java
+++ src/com/ArrayList/ArrayLists.java
@@ -10,6 +10,7 @@ public class ArrayLists {
         list.add(10);
         list.add(20);
         list.add(30);
+        System.out.println("size : " + list.size());
 
         // add at index
         list.add(1, 40);
